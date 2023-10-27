f1a73373e99bcfdd4b374631f93d3d23a043d9e3
Derek Marion <derek.j.marion@gmail.com>
2023-10-27 11:57:43 -0700

    Add randomization to API GET

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const button = document.querySelector("input");
 button.addEventListener("click", catchEm);
 
 function getRandomArbitrary(min, max) {
-    return Math.random() * (max - min) + min;
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
 function catchEm() {
-  randomize = getRandomArbitrary(0, 100) 
-    fetch(`https://pokeapi.co/api/v2/pokemon/${String(randomize)}/`)
+  randomize = getRandomArbitrary(0, 100);
+  fetch(`https://pokeapi.co/api/v2/pokemon/${String(randomize)}/`)
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
