fce7b4915c9ee82e6a980372bb388304ef8d4880
Derek Marion <derek.j.marion@gmail.com>
2023-10-27 13:03:07 -0700

    refactor to use axios for HTTP requests

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-const button = document.querySelector("input");
+const button = document.getElementById("button");
 
 function getRandomArbitrary(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
-const catchEm = async () => {
-  randomize = getRandomArbitrary(0, 100);
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${String(randomize)}/`);
-    const data = await response.json();
-    console.log(data);
+function catchEm() {
+    randomize = getRandomArbitrary(0, 100);
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${String(randomize)}/`) //refactor to not include id in request, see axios docs
+    .then(function (response) {
+        console.log(response);
+        //show picture, name, and type of pokemon
+        const name = response.data.name;
+        const type = function getTypes() {
+            
+        }
+        console.log(name, type);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .finally(function () {
+      // always executed
+    });
+    // let text = document.createTextNode(`You caught a pokemon with id ${id}`);
+    // document.body.appendChild(text);
 }
 
 button.addEventListener("click", catchEm);
